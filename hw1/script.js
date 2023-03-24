abe29386a1df8b14121b9312d0f3f74215cf11af
Oksana Novitska <novitska1803@gmail.com>
2023-03-24 11:00:08 +0200

    first commit

---
+++ hw1/script.js
@@ -0,0 +1,30 @@
+//1) Отримати 3 числових значення і вивести в консоль середнє арефметичне значення
+
+let firstValue = parseInt(prompt("Enter the first value: "));
+let secondValue = parseInt(prompt("Enter the second value: "));
+let thirdValue = parseInt(prompt("Enter the third value: "));
+
+let average = (firstValue + secondValue + thirdValue) / 3;
+
+console.log("Average is: ", average);
+
+
+//2) Ввести 2 значення і порівняти чи вони рівні і вивести в консоль резултат порівння
+
+console.log(true===false);
+console.log(1===1);
+console.log(1=='1');
+console.log(1==='1');
+console.log('a'=='b');
+console.log('a'==='a');
+
+
+/*3) Отримати від користувача розмір знижки(цифрове значення) , 
+і вирахувати ціну з отриманою знижкою, товара який коштує 12999.  
+Значення розрахунку вивести в консоль*/
+
+let discount = parseInt(prompt("Enter the discount amount: "));
+let price = 12999;
+
+let discPrice = price - discount;
+console.log(discPrice);
